26cbc4e9c30477cab71e7c17d66a332c48bd5e06
rosejam <dongju.alex@gmail.com>
2019-08-27 19:01:42 +0900

    0827

--- work_algo/Algo/src/dcbacktracking/Main_정올_1335_색종이만들기_서울9반_김동주_미완.java
+++ work_algo/Algo/src/dcbacktracking/Main_정올_1335_색종이만들기_서울9반_김동주_미완.java
@@ -3,7 +3,9 @@ package dcbacktracking;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main_정올_1335_색종이만들기_서울9반_김동주_미완 {
@@ -14,7 +16,7 @@ public class Main_정올_1335_색종이만들기_서울9반_김동주_미완 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		//StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine()); //2^1~2^7
-		int M = (int) (Math.log(N)/Math.log(2)); //M번쪼개면 1개로 나뉘어짐
+		int M = (int) (Math.log(N)/Math.log(2)); //M번 쪼개면(4부분으로 나누면) 1개씩으로 나뉘어짐
 		blue = new boolean[N][N];
 		for(int i=0; i<N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -25,17 +27,39 @@ public class Main_정올_1335_색종이만들기_서울9반_김동주_미완 {
 			}
 		} //내용 다 가져옴
 		//System.out.println(Arrays.toString(blue[7]));
-		//쪼개기 시작
-		for(int m=M-1; m>=0; m--) {//2^M-1~2^0개
+		//작은 부분부터
+		for(int m=M-1; m>=0; m--) {//2^(M-1)~2^0개의 조각
 			for(int n=0;n<M-m;n++){ //2^1~2^M크기
-				
+				//dividing(m, );
 			}
 		}
-		
+		dividing(blue, M);
 		System.out.println(ws);//하양
 		System.out.print(bs); //파랑
 	}
-	public static void dividing() {
+	public static boolean[][] dividing(boolean[][] whole, int m) { //변이 2^m을넣는다.
+		if(m==1) return whole;
+		boolean[][] quarter1 = new boolean[m/2][m/2];
+		boolean[][] quarter2 = new boolean[m/2][m/2];
+		boolean[][] quarter3 = new boolean[m/2][m/2];
+		boolean[][] quarter4 = new boolean[m/2][m/2];
+		for(int x=0; x<4; x++) {
+			dividing(half, m-1);
+		}
+/*		if(m.size()==1) return m;
+		List<Integer> left = new ArrayList<>();
+		List<Integer> right = new ArrayList<>();
+		int mid = m.size()/2; //분할
+		
+		for(int i=0; 	i<mid; 		i++) left.add(m.get(i));
+		for(int i=mid; 	i<m.size(); i++) right.add(m.get(i));
+		
+		left = mergesort(left); //정복
+		right = mergesort(right); //정복
+		return merge(left, right); //병합
+		//System.out.println(Arrays.toString(a));
+*/	}
+	public static boolean[][] merging() {
 		
 	}
 	public static boolean ifsame() {
---
+++ work_algo/Algo/src/보충/Solution_Ad모의1.java
@@ -0,0 +1,215 @@
+package 보충;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+*/
+class Solution_Ad모의1 //1 ~ N/2 번째 카드는 왼쪽 (L), N/2+1 ~ N 번째 카드는 오른쪽 (R)
+{	
+	static int Ans,T,N;
+	static int[] ccards, oans, nans,cards;
+	static int[] LL,RR,L,R;
+	static boolean bo,bn,notsameo, notsamen;
+	static int co,cn,mino,minn;
+	static int yo,yn;
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 sample_input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 sample_input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		*/
+		System.setIn(new java.io.FileInputStream("res/sample_input_1.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		*/
+		//Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+		T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++)
+		{
+			N = Integer.parseInt(br.readLine()); //카드갯수
+			LL = new int[N/2];
+			RR = new int[N/2];
+			L = new int[N/2];
+			R = new int[N/2];
+			ccards = new int[N];
+			cards = new int[N];
+			oans = new int[N];
+			nans = new int[N];
+			//int x=0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+/*			while(st.hasMoreTokens()) {
+				L[x++]=Integer.parseInt(st.nextToken());
+			}
+			
+*/			for(int n=0; n<N;n++) {
+				ccards[n]=Integer.parseInt(st.nextToken());
+			}
+			for(int n=0; n<N/2;n++) {
+				LL[n]=ccards[n];
+			}
+			for(int n=0; n<N/2;n++) {
+				RR[n]=ccards[n+N/2];
+			}
+			//System.out.println(Arrays.toString(L)+Arrays.toString(R));
+			for(int n=0; n<N; n++) {
+				oans[n]= ccards[n];
+			}
+			Arrays.sort(oans);
+			for(int n=0; n<N; n++) {
+				nans[n]= oans[N-1-n];
+			}
+			//5회 셔플이내에 오름차순or내림차순 정렬
+			//lllrrr, llrlrr, lrlrlr, rlrlrl, rrlrll, rrrlll
+			//123456, 124356, 142536, 
+			bo=false;co=0;yo=0;
+			mino=Integer.MAX_VALUE;
+			notsameo=false;
+			notsamen=false;
+			orum(ccards, LL,RR);
+
+			Ans=0;
+			if(!bo && !bn) {Ans = -1;} //둘다없//정렬이 불가능한 경우나 셔플 횟수가 5 회를 초과할 경우, 정답으로 -1 을 출력한다.
+			else if(!bn) {Ans=mino;} //오름만
+			else if(!bo) {Ans=minn;} //내림만
+			else { //둘다가능하면 적은것
+				if(mino<minn) Ans=mino;
+				else Ans = minn;
+			}
+			// 표준출력(화면)으로 답안을 출력합니다.
+			sb.append("#" + test_case+" "+Ans+"\n"); //정렬에 필요한 최소 셔플 횟수
+		}
+		System.out.println(sb);
+		br.close(); // 사용이 끝난 스캐너 객체를 닫습니다.
+	}
+	private static void orum(int[] cards,int[] L,int[] R) {
+		if(co == 5) return;
+		for(int n=0; n<N; n++) {
+			if(cards[n]!= oans[n]) notsameo=true;
+			if(cards[n]!= nans[n]) notsamen=true;
+		}
+		if(!notsameo||!notsamen) {
+			if(co<mino) mino=co;
+			//mino=co;
+			bo = true;
+			return;
+		}
+		/////////mixing
+		for(int x=0; x<N/2; x++) {
+			for(int n=0;n<N/2-x; n++) { //0
+				cards[n]=L[n];
+			}
+			for(int n=N/2-x; n<N/2+x; n++) { //1,2,3,4
+				if((n-(N/2-x))%2==0) cards[n]=R[(n-(N/2-x))/2];
+				else cards[n]=L[(n-(N/2-x))/2+(N/2-x)];
+			}
+			for(int n=N/2+x;n<N; n++) { //5
+				cards[n]=R[n-N/2]; //2
+			}
+			//
+			for(int n=0; n<N/2;n++) {
+				L[n]=cards[n];
+			}
+			for(int n=0; n<N/2;n++) {
+				R[n]=cards[n+N/2];
+			}
+			co++;
+			orum(cards,L,R);
+			co--;
+		}
+		for(int x=N/2; x<N; x++) {
+			int xx = N-x-1;
+			for(int n=0;n<N/2-xx; n++) { //0
+				cards[n]=R[n];
+			}
+			for(int n=N/2-xx; n<N/2+xx; n++) { //1,2,3,4
+				if((n-(N/2-xx))%2==0) cards[n]=L[(n-(N/2-xx))/2];
+				else cards[n]=R[(n-(N/2-xx))/2+(N/2-xx)];
+			}
+			for(int n=N/2+xx;n<N; n++) { //5
+				cards[n]=L[n-N/2]; //2
+			}
+			//
+			for(int n=0; n<N/2;n++) {
+				L[n]=cards[n];
+			}
+			for(int n=0; n<N/2;n++) {
+				R[n]=cards[n+N/2];
+			}
+			co++;
+			orum(cards,L,R);
+			co--;
+		}
+	}
+	private static void nerim(int[] cards,int[] L,int[] R) {
+		if(cn == 5) return;
+		for(int n=0; n<N; n++) {
+			if(cards[n]!= oans[n]) notsameo=true;
+			if(cards[n]!= nans[n]) notsamen=true;
+		}
+		if(!notsameo||!notsamen) {
+			if(cn<minn) minn=cn;
+			bn = true;
+			return;
+		}
+		/////////mixing
+		for(int x=0; x<N/2; x++) {
+			for(int n=0;n<N/2-x; n++) { //0
+				cards[n]=L[n];
+			}
+			for(int n=N/2-x; n<N/2+x; n++) { //1,2,3,4
+				if((n-(N/2-x))%2==0) cards[n]=R[(n-(N/2-x))/2];
+				else cards[n]=L[(n-(N/2-x))/2+(N/2-x)];
+			}
+			for(int n=N/2+x;n<N; n++) { //5
+				cards[n]=R[n-N/2]; //2
+			}
+			//
+			for(int n=0; n<N/2;n++) {
+				L[n]=cards[n];
+			}
+			for(int n=0; n<N/2;n++) {
+				R[n]=cards[n+N/2];
+			}
+			cn++;
+			nerim(cards,L,R);
+			cn--;
+		}
+		for(int x=N/2; x<N; x++) {
+			int xx = N-x-1;
+			for(int n=0;n<N/2-xx; n++) { //0
+				cards[n]=R[n];
+			}
+			for(int n=N/2-xx; n<N/2+xx; n++) { //1,2,3,4
+				if((n-(N/2-xx))%2==0) cards[n]=L[(n-(N/2-xx))/2];
+				else cards[n]=R[(n-(N/2-xx))/2+(N/2-xx)];
+			}
+			for(int n=N/2+xx;n<N; n++) { //5
+				cards[n]=L[n-N/2]; //2
+			}
+			//
+			for(int n=0; n<N/2;n++) {
+				L[n]=cards[n];
+			}
+			for(int n=0; n<N/2;n++) {
+				R[n]=cards[n+N/2];
+			}
+			cn++;
+			nerim(cards,L,R);
+			cn--;
+		}
+	}
+}
