77c3f229efe2edbbe5e410b3ed42f8d0387d7c5b
rosejam <dongju.alex@gmail.com>
2019-09-06 17:48:42 +0900

    0906end

--- work_java/hw/hwjava11_서울_9반_김동주/src/com/ssafy/news/INewsDAO.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.news;
-
-import java.util.List;
-
-public interface INewsDAO {
-	List<News> getNewsList(String url); //xml받아와서 parsing후 VO(News)에 담는다.
-	public News search(int index);
-}
