8ecaccdfaac1189ef1629215814e60ba4861dfc4
rosejam <dongju.alex@gmail.com>
2019-10-07 18:00:07 +0900

    1007

---
+++ work_algo/Algo/src/sw기출/Main_BJ_16234_인구이동_서울9반_김동주.java
@@ -0,0 +1,15 @@
+package sw기출;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+
+public class Main_BJ_16234_인구이동_서울9반_김동주 {
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new Bu
+				
+
+	}
+
+}
