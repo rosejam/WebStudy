f0c2a9b2658923009bfcc3188a6298adbb675262
rosejam <dongju.alex@gmail.com>
2019-10-31 14:38:30 +0900

    1031

--- work_algo/Algo/src/dp/TSPTest2.java
+++ work_algo/Algo/src/dp/TSPTest2.java
@@ -3,20 +3,20 @@ package dp;
 import java.io.*;
 import java.util.*;
 
-public class TSPTest {
+public class TSPTest2 {
 	public static int INF=987654321;
 	public static int N;
 	public static int[][] G;
 	public static int[][] d;
 	//
-	//public static int[][] m;//메모이제이션용!
+	public static int[][] m;//메모이제이션용!
 	
 	public static int solve(int pos, int visited, int start) {
 		//
-		//if(m[pos][visited]!=0) return m[pos][visited];//
-		if(visited==(1<<N)-1) //
-			return d[pos][start]; //return m[pos][visited]=d[pos][start];//
-			
+		if(m[pos][visited]!=0) return m[pos][visited];//
+		if(visited==(1<<N)-1) ////return d[pos][start]; //
+			return m[pos][visited]=d[pos][start];//
+		
 		int ret=INF;
 		for(int next=0; next<N; next++) {
 			if((visited&(1<<next))==0 && d[pos][next]!=0){
@@ -25,7 +25,7 @@ public class TSPTest {
 			}
 		}
 		//
-		//m[pos][visited]=ret;
+		m[pos][visited]=ret;//
 		//for(int[] a:m) System.out.println(Arrays.toString(a));
 		//System.out.println();
 		return ret;
@@ -52,10 +52,10 @@ public class TSPTest {
 					d[j]=G[j].clone();
 				}
 				//
-				//m=new int[N][1<<N];//
-				int tmp=solve(i,1<<i,i);
-				ans=Math.min(ans,tmp);
 			}
+			m=new int[N][1<<N];// 2^N 길이의 메모이제이션 배열을 만든다.
+			int tmp=solve(0,1<<0,0);//JO_1545용		//int tmp=solve(i,1<<i,i);
+			ans=Math.min(ans,tmp);
 			
 			System.out.println("#"+tc+" "+ans);
 		}
