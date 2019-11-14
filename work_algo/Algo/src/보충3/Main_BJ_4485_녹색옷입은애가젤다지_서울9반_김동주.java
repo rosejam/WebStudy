c2ea2fccdb72012eff0479f930a841abffec0a76
rosejam <dongju.alex@gmail.com>
2019-11-15 08:20:58 +0900

    t

---
+++ work_algo/Algo/src/보충3/Main_BJ_4485_녹색옷입은애가젤다지_서울9반_김동주.java
@@ -0,0 +1,11 @@
+package 보충3;
+
+//dfs(97넘으므로 반드시 시간초과), bfs(전체 탐색 해봐야됨)둘다 안됨!!!!
+public class Main_BJ_4485_녹색옷입은애가젤다지_서울9반_김동주 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
