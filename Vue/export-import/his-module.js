2cb64aab08f6c5d60184253447181b5f9d30b410
rosejam <dongju.alex@gmail.com>
2019-11-15 12:39:01 +0900

    1115

---
+++ Vue/export-import/his-module.js
@@ -0,0 +1,13 @@
+export function gogo(){
+
+}
+export var nums = 789;
+
+/*export default {
+    number: 1129,
+    color: 'purple',
+    name: '김동주'
+}*/
+export default function(){
+    return "hello, vue"
+}
